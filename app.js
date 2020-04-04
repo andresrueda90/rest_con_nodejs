97eb60ead73ad6d0a06574c6d84a27ab47086b08
andresrueda90 <andresrueda90@gmail.com>
2020-04-04 10:32:07 -0500

    manejo de rutas video 24

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 
-var app = express();
+
 const Place = require('./models/Place')
+const routePlace = require('./routes/places')
 const db = require('./config/database')
+
+
+var app = express();
 db.connect();
 
 app.use(logger('dev'));
@@ -15,46 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/places',routePlace)
 
-
-
-app.post('/places',(req,res)=>{
-  Place.create({
-    title: 'Nuevo lugar',
-    description:'Nuevo lugar creado curso',
-    acceptsCreditCard:false,
-    coverImage:'',
-    avatarImage:'',
-    openHour:0,
-    closeHour:0
-  }).then((doc)=>{res.json(doc)}).catch((error)=>{res.json(error)})
-})
-
-
-app.get('/places',(req,res)=>{
-  Place.find({}).then((doc)=>{res.json(doc)}).catch((error)=>{res.json(error)})
-})
-
-app.get('/places/:id',(req,res)=>{
-  Place.findOne({'_id':req.params.id}).then((doc)=>{res.json(doc)}).catch((error)=>{res.json(error)})
-})
-
-app.put('/places/:id',(req,res)=>{
-  Place.update({'_id':req.params.id},{
-    title: 'Nuevo lugar modificado',
-    description:'Nuevo lugar creado curso Modificado',
-    acceptsCreditCard:false,
-    coverImage:'eeee',
-    avatarImage:'wwww',
-    openHour:2,
-    closeHour:3
-  }).then((doc)=>{res.json(doc)}).catch((error)=>{res.json(error)})
-})
-
-
-app.delete('/places/:id',(req,res)=>{
-  Place.findByIdAndRemove(req.params.id).then((doc)=>{res.json(doc)}).catch((error)=>{res.json(error)})
-})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
